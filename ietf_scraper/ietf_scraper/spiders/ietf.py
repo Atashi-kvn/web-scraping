dcd082318e20121a9dda9edaeb4b98075dae53c3
Kelvin Atandi <ataskv.gov@gmail.com>
2024-02-08 23:37:06 +0300

    Web crawler

--- ietf_scraper/ietf_scraper/spiders/ietf.py
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -8,6 +8,17 @@ class IetfSpider(scrapy.Spider):
 
     def parse(self, response):
         # title = response.css('span, title::text').get()
-        title = response.xpath('//span[@class="title"]/text()').get()
-        return {"title": title}
+        # title = response.xpath('//span[@class="title"]/text()').get()
+        return {
+            "number": response.xpath('//span[@class="rfc-no"]/text()').get(),
+            "title": response.xpath('//meta[@name="DC.Date.Title"]/@content'),
+            "author": response.xpath('//meta[@name="DC.Creator"]/@content'),
+            'date': response.xpath('//span[@class="date"]/text()').get(),
+            'description': response.xpath('//meta[@name="DC.Description"]/@content'),
+            'company': response.xpath('//span[@class="author-name"]/text()').get(),
+            'address': response.xpath('//span[@class="address"]/text()').get(),
+            'headings': response.xpath('//span[@class="subheading"]/text()').get(),
+            'text': w3lib.html.remove_tags(response.xpath('//div[@class="text"]').get()),
+            
+            }
         # pass
