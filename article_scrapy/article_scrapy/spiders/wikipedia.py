484cb7bf2c2da6c8e5930a0836c00eb7c6b0fefc
Kelvin Atandi <ataskv.gov@gmail.com>
2024-02-09 00:21:48 +0300

    crawler updates

--- article_scrapy/article_scrapy/spiders/wikipedia.py
+++ article_scrapy/article_scrapy/spiders/wikipedia.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from article_scrapy.items import Article
 
 
 
@@ -9,10 +10,10 @@ class WikipediaSpider(CrawlSpider):
     allowed_domains = ["en.wikipedia.org"]
     start_urls = ["https://en.wikipedia.org/wiki/Kevin_Bacon"]
 
-    rules = [Rule(LinkExtractor(), callback='parse_info', follow=True)]
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
     def parse_info(self, response):
-        return{
-            'title': response.xpath('//h1/text()').get(),
-            'url': response.url,
-            'last_edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get(),
-        }
+        article = Article()
+        article['title'] = response.xpath('//h1/text()').get(),
+        article['url'] = response.url,
+        article['last_edited'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get(),
+        return article
